7366461e2673d81258a664ee6affaeeaff5369f3
nitin <snitin8994@gmail.com>
2019-06-13 09:19:11 +0530

    Change algolia api url

--- src/utils/fetchAlgoliaSearchResults.js
+++ src/utils/fetchAlgoliaSearchResults.js
@@ -1,6 +1,6 @@
 export const fetchAlgoliaSearchResults=(query,page)=> {
     return fetch(
-      `http://hn.algolia.com/api/v1/search?query=${query}&tags=story&page=${page}`
+      `https://hn.algolia.com/api/v1/search?query=${query}&tags=story&page=${page}`
     ).then(resp=>resp.json())
 
 }
